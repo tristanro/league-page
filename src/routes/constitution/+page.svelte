3a8d490c61c61204280a530562a5af154b4ae24d
Tristan Ro <90853116+tristanro@users.noreply.github.com>
2024-08-26 21:44:54 -0400

    updated to remove positional max

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -169,19 +169,6 @@
     <p>6 Bench Slots</p>
 
     <p>2 IR Spots (Player must be labeled as IR, Out, or Covid in the Sleeper App)</p>
-    
-        <h4 bind:this={oneOneOne}>1.1.1 Position Maximums</h4>
-        
-        <div class="subBlock">
-            <table class="positionMaximums">
-                <tr><td>QB</td><td class="right">3 active, 5 total</td></tr>
-                <tr><td>RB</td><td class="right">20</td></tr>
-                <tr><td>WR</td><td class="right">20</td></tr>
-                <tr><td>TE</td><td class="right">10</td></tr>
-                <tr><td>D/ST</td><td class="right">3</td></tr>
-                <tr><td>K</td><td class="right">3</td></tr>
-            </table>
-        </div>
 
 
     <h3 class="subSectionHeading" bind:this={oneTwo}>1.2 Trading</h3>
